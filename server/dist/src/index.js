8148c15a2833042a043005f5ce6337873408a943
nevermin9 <chizzza@gmail.com>
2025-05-21 14:11:09 +0200

    create server; added basic ts config

---
+++ server/dist/src/index.js
@@ -0,0 +1,7 @@
+const myName = "Anton Chistik";
+const say_my_name = (name) => {
+    console.log(`Hello, ${name}`);
+};
+say_my_name(myName);
+export {};
+//# sourceMappingURL=index.js.map
---
+++ server/src/index.ts
@@ -0,0 +1,7 @@
+const myName: string = "Bruce Willis";
+
+const say_my_name = (name: string) => {
+    console.log(`Hello, ${name}`);
+};
+
+say_my_name(myName);
